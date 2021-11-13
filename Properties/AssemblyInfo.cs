5f52860bf25121c624a6647171f3b74c6e626660
kokkiemouse <kokkiemouse@fascode.net>
2021-11-14 08:28:11 +0900

    fixed

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("UST_MUSICXML_LIB")]
 [assembly: AssemblyDescription("ustファイルをmusicxmlにするやつ。")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("kokkiemouse")]
+[assembly: AssemblyCompany("kahenteikou")]
 [assembly: AssemblyProduct("UST_MUSICXML_LIB")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
